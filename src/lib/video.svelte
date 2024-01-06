39c5f4109516cc3fe22ff486f22432eebcd563c4
hutchisr <42283663+hutchisr@users.noreply.github.com>
2024-01-05 19:51:37 -0800

    Fix formatting
    the default is HORRIBLE

--- src/lib/video.svelte
+++ src/lib/video.svelte
@@ -1,42 +1,42 @@
 <script lang="ts">
-	import '/node_modules/plyr/dist/plyr.css';
-	import { onMount } from 'svelte';
+  import "/node_modules/plyr/dist/plyr.css";
+  import { onMount } from "svelte";
 
-	let player: Plyr;
+  let player: Plyr;
 
-	export let src: string;
+  export let src: string;
 
-	onMount(async () => {
-		const Plyr = (await import('plyr')).default;
-		const Hls = (await import('hls.js')).default;
-		const video = document.querySelector('#video-player');
-		player = new Plyr('#video-player', {
-			autoplay: true,
-			muted: true,
-			controls: ['play-large', 'fullscreen', 'pip', 'airplay'],
-		});
-		if (Hls.isSupported()) {
-			// For more Hls.js options, see https://github.com/dailymotion/hls.js
-			const hls = new Hls();
-			hls.loadSource(src);
-			hls.attachMedia(video as HTMLMediaElement);
-		}
-		player.volume = 0;
-		player.forward(Infinity);
-		player.play();
+  onMount(async () => {
+    const Plyr = (await import("plyr")).default;
+    const Hls = (await import("hls.js")).default;
+    const video = document.querySelector("#video-player");
+    player = new Plyr("#video-player", {
+      autoplay: true,
+      muted: true,
+      controls: ["play-large", "fullscreen", "pip", "airplay"]
+    });
+    if (Hls.isSupported()) {
+      // For more Hls.js options, see https://github.com/dailymotion/hls.js
+      const hls = new Hls();
+      hls.loadSource(src);
+      hls.attachMedia(video as HTMLMediaElement);
+    }
+    player.volume = 0;
+    player.forward(Infinity);
+    player.play();
 
-		player.on('error', () => player.restart())
-	});
+    player.on("error", () => player.restart());
+  });
 
-	function onFocus() {
-		player.forward(Infinity);
-		player.play();
-	}
+  function onFocus() {
+    player.forward(Infinity);
+    player.play();
+  }
 </script>
 
 <svelte:window on:focus={onFocus} />
 
 <video id="video-player" autoplay muted>
-	<source {src} />
-	<track kind="captions" />
+  <source {src} />
+  <track kind="captions" />
 </video>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    import Video from '$lib/video.svelte';
-    let logo = '/shrimpicon/apple-icon-76x76.png';
-    let src = "https://shrimpcam.app/live/shrimpcam/index.m3u8";
+  import Video from "$lib/video.svelte";
+  let logo = "/shrimpicon/apple-icon-76x76.png";
+  let src = "https://shrimpcam.app/live/shrimpcam/index.m3u8";
 </script>
 
 <h1 class="text-center">Shrimpcam <img src={logo} alt="Shrimp Icon" /></h1>
 <Video {src} />
 
 <svelte:head>
-    <title>Shrimpcam</title>
+  <title>Shrimpcam</title>
 </svelte:head>
